ef2970e27d7deaf2c402edc374d5f2691e7a26af
Andrea Creazzi <andreacreazzi@libero.it>
2023-06-09 15:00:23 +0200

    add btn (event listner) + randomArr

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 // controllo js
 console.log(`JS OK`)
+
+// prendo gli elementi dal DOM
+const button = document.querySelector(`button`);
+console.log(button);
+
+const timerElement = document.getElementById(`timer`);
+console.log(timerElement);
+
+const numbersElement = document.getElementById(`numbers`);
+console.log(numbersElement);
+
+// bottone in ascolto
+button.addEventListener(`click` , function(){
+    // variabile con array e numeri randomici
+    const randomArr = Array.from({length: 10}, () => Math.floor(Math.random() * 10))
+    console.log(randomArr)
+})
